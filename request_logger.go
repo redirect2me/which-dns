60d6eeda291e17a88c3b2c5d231b64be68a737ca
Andrew Marcuse <fileformat@gmail.com>
2022-05-01 15:48:46 -0400

    Added missing source file for logging

---
+++ request_logger.go
@@ -0,0 +1,67 @@
+package main
+
+// request_logger.go
+// adapted from https://blog.questionable.services/article/guide-logging-middleware-go/
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+// responseWriter is a minimal wrapper for http.ResponseWriter that allows the
+// written HTTP status code to be captured for logging.
+type responseWriter struct {
+	http.ResponseWriter
+	status      int
+	wroteHeader bool
+}
+
+func wrapResponseWriter(w http.ResponseWriter) *responseWriter {
+	return &responseWriter{ResponseWriter: w, status: 200}
+}
+
+func (rw *responseWriter) Status() int {
+	return rw.status
+}
+
+func (rw *responseWriter) WriteHeader(code int) {
+	if rw.wroteHeader {
+		return
+	}
+
+	rw.status = code
+	rw.ResponseWriter.WriteHeader(code)
+	rw.wroteHeader = true
+
+	return
+}
+
+// LoggingMiddleware logs the incoming HTTP request & its duration.
+func LoggingMiddleware(logger log.Logger) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		fn := func(w http.ResponseWriter, r *http.Request) {
+			defer func() {
+				if err := recover(); err != nil {
+					w.WriteHeader(http.StatusInternalServerError)
+					logger.Printf("ERROR: request failed (%s)\n", err)
+					//"err", err,
+					//"trace", debug.Stack(),
+					//)
+				}
+			}()
+
+			start := time.Now()
+			wrapped := wrapResponseWriter(w)
+			next.ServeHTTP(wrapped, r)
+			logger.Printf("LOG: %d %s %s %s %d",
+				wrapped.status,
+				r.Method,
+				r.Host,
+				r.URL.EscapedPath(),
+				time.Since(start),
+			)
+		}
+
+		return http.HandlerFunc(fn)
+	}
+}
